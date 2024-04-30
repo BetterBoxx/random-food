ff3ffb9974a021508fb645aedad50e7075747ee6
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2024-04-30 16:46:14 +0900

    🎨 Accept multiple commands and use common constant

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 import fs from "fs";
 
+// Constants
+let randomFoodItem = "";
+const ketoFoodList = JSON.parse(
+	fs.readFileSync("./plugins/random-food/ketoDB.json", "utf8")
+);
+const foodList = JSON.parse(
+	fs.readFileSync("./plugins/random-food/foodDB.json", "utf8")
+);
+const suffixList = JSON.parse(
+	fs.readFileSync("./plugins/random-food/suffixDB.json", "utf8")
+);
+
 export function randomFood(msg) {
-	let randomFoodItem = "";
-	const ketoFoodList = JSON.parse(
-		fs.readFileSync("./plugins/random-food/ketoDB.json", "utf8")
-	);
-	const foodList = JSON.parse(
-		fs.readFileSync("./plugins/random-food/foodDB.json", "utf8")
-	);
-	const suffixList = JSON.parse(
-		fs.readFileSync("./plugins/random-food/suffixDB.json", "utf8")
-	);
+	if (
+		msg.content.startsWith(process.env.RANDOM_FOOD_1) ||
+		msg.content.startsWith(process.env.RANDOM_FOOD_2)
+	) {
+		if (msg.room.startsWith("KETO")) {
+			const randomKetoFoodIndex = Math.floor(Math.random() * ketoFoodList.length);
+			randomFoodItem = ketoFoodList[randomKetoFoodIndex].name;
+		} else {
+			const randomFoodIndex = Math.floor(Math.random() * foodList.length);
+			randomFoodItem = foodList[randomFoodIndex].name;
+		}
 
-	if (msg.room.startsWith("KETO")) {
-		const randomKetoFoodIndex = Math.floor(Math.random() * ketoFoodList.length);
-		randomFoodItem = ketoFoodList[randomKetoFoodIndex].name;
-	} else {
-		const randomFoodIndex = Math.floor(Math.random() * foodList.length);
-		randomFoodItem = foodList[randomFoodIndex].name;
+		const randomSuffixIndex = Math.floor(Math.random() * suffixList.length);
+		const randomSuffix = suffixList[randomSuffixIndex].name;
+		const generatedMessage = randomFoodItem + randomSuffix;
+
+		msg.reply(generatedMessage);
 	}
-
-	const randomSuffixIndex = Math.floor(Math.random() * suffixList.length);
-	const randomSuffix = suffixList[randomSuffixIndex].name;
-	const generatedMessage = randomFoodItem + randomSuffix;
-
-	msg.reply(generatedMessage);
 }
